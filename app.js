64978f4779f5fc691fb885ca84c8829a3663dfbc
teacarolina <teacarolina@icloud.com>
2020-11-24 15:30:49 +0100

    Finished function for monthly budget in js, base page is finished

--- app.js
+++ app.js
@@ -1,38 +1,68 @@
-//för att det inte skulle uppdatera sig? e prevent?
 //ska det vara minus och plustecken framför? 
 
-//Function below creates li based on input from user,
-//and pushes it into income- or spendinglist depending on,
-//if user chooses + or - in input option
-function createLi() {
-    var option = document.querySelector("#calculate")
-    var description = document.querySelector("#description")
-    var amount = document.querySelector("#amount")
-    var text1 = description.value
-    var text2 = amount.value
+//Function for monthly budget
 
+//Arrays used to store amount input from users
+var array = []
+var arrayIncome = []
+var arraySpendings = []
+
+function createLi(event) {
+    event.preventDefault(); 
+    var option = document.querySelector("#calculate")
+    var description = document.querySelector("#description").value
+    var amount = document.querySelector("#amount").value
 //+
 //created if statement if the user chooses +
 if(option.value==="+") {
+    //creates li element and pushes it into ul
     var incomeUl = document.querySelector(".income--ul")
     var li = document.createElement("li")
-    li.innerHTML = text1 + " : " + text2
+    li.innerHTML = description + " : " + amount
     incomeUl.appendChild(li)
-}
-
+    //push + numbers into arrays
+    array.push(Number(amount))
+    arrayIncome.push(Number(amount))
+}   
 //-
-//added else to make the function push it into - if
-//user doesn't choose +
+//added else if the user chooses - option
 else {
+    //creates li element and pushes it into ul
     var spendingUl = document.querySelector(".spending--ul")
     var li = document.createElement("li")
-    li.innerHTML = text1 + " : " + text2
+    li.innerHTML = description + " : " + amount
     spendingUl.appendChild(li)
+    //push - numbers into arrays 
+    array.push(Number(-amount))
+    arraySpendings.push(Number(-amount))
+}
+//for loop to show total spendings based on input
+var test = 0;
+for(var i = 0; i<arraySpendings.length; i++) {
+    test += arraySpendings[i];
+}
+//for loop to show total income based on input
+var test2 = 0; 
+for(var i = 0; i<arrayIncome.length; i++) {
+    test2 += arrayIncome[i];
+}
+//for loop to show total profit based on income-spendings
+var total = 0; 
+for(var i = 0; i<array.length; i++) {
+    total += array[i];
+}
+//stores total profit in element
+var vinst  = document.querySelector(".savings--h4")
+vinst.innerHTML = total
+//stores total spendings in element
+var totalSpendings = document.querySelector(".spendings--p")
+totalSpendings.innerHTML = test
+//stores total income in element
+var totalIncome = document.querySelector(".income--p")
+totalIncome.innerHTML = test2    
 }
 
-}
-
-//added eventlistner to button so it creates li when
+//added eventlistner to button so it creates the function when
 //button is clicked
 var btn = document.querySelector("#button")
 btn.addEventListener("click", createLi)
